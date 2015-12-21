92718049040c350c421cce0cff92f1b3e1656134
rarila <magnet@gmx.net>
2015-12-21 02:49:33 +0100

    Add buicPassword widget stub

---
+++ app/src/js/widgets/buic/buicPassword.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Object} $ - Global jQuery object
+ */
+(function ($) {
+    'use strict';
+
+    /**
+     * BUIC checkbox widget.
+     *
+     * @license http://opensource.org/licenses/mit-license.php MIT License
+     * @author rarila
+     *
+     * @class buicPassword
+     * @memberOf jQuery.widget.bolt
+     */
+    $.widget('bolt.buicPassword', /** @lends jQuery.widget.bolt.buicPassword.prototype */ {
+        /**
+         * The constructor of the password widget.
+         *
+         * @private
+         */
+        _create: function () {
+        }
+    });
+})(jQuery);
